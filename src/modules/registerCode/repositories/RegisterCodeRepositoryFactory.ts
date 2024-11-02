9f150e427114a08dcdd1b8a26a36d846420b0521
ashkan-101 <ashkan.dev.101@gmail.com>
2024-11-02 03:34:13 +0330

    fixed modules/registerCode

--- src/modules/registerCode/repositories/RegisterCodeRepositoryFactory.ts
+++ src/modules/registerCode/repositories/RegisterCodeRepositoryFactory.ts
@@ -1,13 +1,13 @@
 import DatabaseName from "../../contracts/DatabaseName";
-import RegisterCodeMongoRepository from "./RegisterCodeMongoRepository";
 import RegisterCodePGRepository from "./RegisterCodePGRepository";
-import IRepository from "../../contracts/IRepository";
-import IBaseRegisterCode from "../model/contracts/IBaseRegisterCode";
+import RegisterCodeMongoRepository from "./RegisterCodeMongoRepository";
+import IRegisterCodePGRepository from "./contracts/IRegisterCodePGRepository";
+import IRegisterCodeMongoRepository from "./contracts/IRegisterCodeMongoRepository";
 
 
 export default class RegisterCodeRepositoryFactory {
-  private repository: Map<DatabaseName, IRepository<IBaseRegisterCode>> = new Map<DatabaseName, IRepository<IBaseRegisterCode>>()
-
+  private repository: Map<DatabaseName, IRegisterCodeMongoRepository | IRegisterCodePGRepository> = new Map<DatabaseName, IRegisterCodeMongoRepository | IRegisterCodePGRepository>()
+  
   constructor(){
     this.repository.set(DatabaseName.MONGODB, new RegisterCodeMongoRepository())
     this.repository.set(DatabaseName.POSTGRES, new RegisterCodePGRepository())
--- src/modules/registerCode/repositories/contracts/IRegisterCodePGRepository.ts
+++ src/modules/registerCode/repositories/contracts/IRegisterCodePGRepository.ts
@@ -1,7 +1,6 @@
 import IRepository from "../../../contracts/IRepository";
 import IRegisterCodePG from "../../model/contracts/IRegisterCodePG";
 
-
 export default interface IRegisterCodePGRepository extends IRepository<IRegisterCodePG>{
   findWithParams(params: Partial<IRegisterCodePG>): Promise<null | IRegisterCodePG>
 }
